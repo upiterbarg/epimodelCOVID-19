36004dc87460f8218d0e3173a4355f4f850c8775
Ulyana Piterbarg <ulyana@ulyanas-mbp.lan>
2020-03-25 16:24:54 -0400

    set up bayesian inference on SIR model

--- data.py
+++ data.py
@@ -1,5 +1,5 @@
 import numpy as np 
-import h5py
+import pandas as pd
 import os
 import os.path as osp
 import pdb
@@ -7,22 +7,31 @@ import csv
 
 
 def unpack_data():
+    '''
+    Accesses the submodule 'COVID-19' and processes data
+
+    *** cloned from the repo maintained by JHU CSSE, available here: 
+    https://github.com/CSSEGISandData/COVID-19
+    '''
+
     # Unpack latest timeseries data
     ts_path = osp.join(os.getcwd(), 'COVID-19', 'csse_covid_19_data', 'csse_covid_19_time_series')
-    deaths_path = osp.join(ts_path, 'time_series_19-covid-Confirmed.csv')
-    conf_path = osp.join(ts_path, 'time_series_19-covid-Deaths.csv')
+    conf_path = osp.join(ts_path, 'time_series_19-covid-Confirmed.csv')
+    deaths_path = osp.join(ts_path, 'time_series_19-covid-Deaths.csv')
     recov_path = osp.join(ts_path, 'time_series_19-covid-Recovered.csv')
+    confglob_path = osp.join(ts_path, 'time_series_covid19_confirmed_global.csv')
+    deathsglob_path = osp.join(ts_path, 'time_series_covid19_deaths_global.csv')
 
     # Load deaths data
     deaths, labels, dlocs = [], [], []
     with open(deaths_path, 'r') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
         for row in reader:
-            if len(labels) == 0: labels=row
+            if len(labels) == 0: labels=row[:-1]
             else:
                 dlocs.append(row[:4])
-                deaths.append([float(r) for r in row[4:]])
-    deaths, dlocs = np.array(deaths), np.array(dlocs)
+                deaths.append([float(r) for r in row[4:-1]])
+    labels, deaths, dlocs = np.array(labels), np.array(deaths), np.array(dlocs)
 
     # Load confirmed cases data
     conf, clocs, f = [], [], False
@@ -32,7 +41,7 @@ def unpack_data():
             if not f: f=True
             else:
                 clocs.append(row[:4])
-                conf.append([float(r) for r in row[4:]])
+                conf.append([float(r) for r in row[4:-1]])
     conf, clocs = np.array(conf), np.array(clocs)
 
     # Load recovered data
@@ -43,9 +52,90 @@ def unpack_data():
             if not f: f=True
             else:
                 rlocs.append(row[:4])
-                recov.append([float(r) for r in row[4:]])
+                recov.append([float(r) for r in row[4:-1]])
     recov, rlocs = np.array(recov), np.array(rlocs)
 
-    dd = {'deaths': deaths, 'conf': conf, 'recov': recov, 'dlocs':dlocs, 'clocs':clocs, 'rlocs':rlocs}
-    return dd, labels
+    # Load global confirmed cases data 
+    confglob, cgloc, labels_global = [], [], None
+    with open(confglob_path, 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            if labels_global is None: labels_global = row
+            else:
+                cgloc.append(row[:4])
+                confglob.append([float(r) for r in row[4:]])
+    labels_global, confglob, cgloc = np.array(labels_global), np.array(confglob), np.array(cgloc)
+
+    # Load global deaths cases data 
+    deathsglob, dgloc, f = [], [], False
+    with open(deathsglob_path, 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            if not f: f=True
+            else:
+                dgloc.append(row[:4])
+                deathsglob.append([float(r) for r in row[4:]])
+    deathsglob, dgloc = np.array(deathsglob), np.array(dgloc)
+
+    # Drop diamond 'princess, canada' and 'recovered, canada' from global data 
+    mask1 = cgloc[:, :2] == ['Diamond Princess','Canada']
+    mask2 = cgloc[:, :2] == ['Recovered','Canada']
+    mask = np.array(mask1+mask2, dtype=int).sum(axis=1) != 2
+    confglob, cgloc = confglob[mask], cgloc[mask]
+    deathsglob, dgloc = deathsglob[mask], dgloc[mask]
+
+    dd = {'deaths': deaths, 'conf': conf, 'recov': recov, 'confglob': confglob, 'deathsglob': deathsglob,
+        'dlocs': dlocs, 'clocs': clocs, 'rlocs': rlocs, 'cgloc': cgloc, 'dgloc': dgloc}
+    return dd, labels, labels_global
+
+def clean_by_country(country, dd, labels, case_thresh=1):
+    '''
+    Extract country-specific according to the specified case threshold ('case_thresh'),
+    returning all relevant values (and corresponding dates) normalized by country population.
+
+    Returns:
+        dates -- (type: str np.array) all relevant dates
+        x -- (type: int np.array) enumeration of data for SIR model
+        infected -- (type: Float32 np.array) fraction of population infected by disease by day
+        susceptible -- (type: Float32 np.array) fraction of population susceptible to disease by day
+         '''
+    dlocs, clocs, rlocs = dd['dlocs'], dd['clocs'], dd['rlocs'] # Unpack location labels (equivalent for )
+    
+    # Sum across all regions (if regional info for country is given)
+    deaths = dd['deaths'][dlocs[:, 1] == country, :].sum(axis=0)
+    conf = dd['conf'][clocs[:, 1] == country, :].sum(axis=0)
+    recov = dd['recov'][rlocs[:, 1] == country, :].sum(axis=0)
+
+    if deaths.shape[0] == 0 or conf.shape[0] == 0 or recov.shape[0] == 0:
+        raise ValueError('Failed to find country in data! Check your spelling.')
+
+    # Apply case threshold
+    mask = conf >= case_thresh
+    min_ind = np.min(np.arange(0, conf.shape[0])[mask])
+    deaths, conf, recov, dates = deaths[min_ind:], conf[min_ind:], recov[min_ind:], labels[4+min_ind:]
+
+    # Convert data (deaths, conf, recov) from corresponding to per day reports to instead
+    # represent cumulative numbers
+    for i in range(1, dates.shape[0]):
+        deaths[i] += deaths[i-1]
+        conf[i] += conf[i-1]
+        recov[i] += recov[i-1]
+
+    # Extract total population from World Bank Data csv (note: based on 2018!!)
+    df_pop = pd.read_csv("population_worldbank2018.csv")
+    pop = df_pop['Population'][df_pop['Country'] == country]
+    if pop.shape[0] == 0:
+        raise ValueError('Failed to find country population in World Bank dataset.')
+    pop = pop.values[0]
+    
+    # Compute fraction of infected and susceptible individuals in country by day
+    infected = (conf - deaths - recov)/pop
+    susceptible = (pop - conf)/pop
+
+    return dates, np.arange(1, deaths.shape[0]+1), infected/pop, susceptible/pop
+
+def clean_by_state(state, dd, labels, case_thresh=10):
+    pass
+
+
 
--- main.py
+++ main.py
@@ -2,59 +2,124 @@ import pymc3 as pm
 from pymc3.ode import DifferentialEquation
 import numpy as np 
 import matplotlib.pyplot as plt
-from scipy.integrate import odeint
+from matplotlib import rc
+from scipy.integrate import odeint, solve_ivp
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 import arviz as az 
 import pdb
 from data import *
+import theano
 
-plt.style.use('seaborn-darkgrid')
+floatX = theano.config.floatX
+
+#plt.style.use('seaborn-darkgrid')
+#rc('font',**{'family':'serif','serif':['Palatino']})
+#rc('text', usetex=True)
 
 # SIR Model
 def SIR(y, t, p):
-    ds = -p[0]*y[0]*y[1]
-    di = p[0]*y[0]*y[1] - p[1]*y[1]
+    ds = -p[0]*y[0]*y[1] # Susceptible differential
+    di = p[0]*y[0]*y[1] - p[1]*y[1] # Infected differential
     return [ds, di]
 
-sir_model = DifferentialEquation(
-    func=SIR,
-    times=np.arange(0.25, 5, 0.25),
-    n_states=2,
-    n_theta=2,
-    t0=0,
-)
+class DE(DifferentialEquation):
+    def _simulate(self, y0, theta):
+        # Begin with initial conditions and raveled sensitivity matrix
+        s0 = np.concatenate([y0, self._sens_ic])
+        
+        # Integrate
+        sol = solve_ivp(
+            fun = lambda t, Y: self._system(Y, t, tuple(np.concatenate([y0, theta]))),
+            t_span=[self._augmented_times.min(), self._augmented_times.max()],
+            y0=s0,
+            method='RK23',
+            t_eval=self._augmented_times[1:],
+            atol=1, rtol=1,
+            max_step=0.02).y.T.astype(floatX)
+        
+        # Extract the solution
+        y = sol[:, :self.n_states]
 
-def main():
-    dd, labels = unpack_data()
-    date_labels = labels[4:]
+        # Reshape sensativities
+        sens = sol[0:, self.n_states:].reshape(self.n_times, self.n_states, self.n_p)
 
-    deaths, conf, recov = dd['deaths'].sum(axis=1), dd['conf'].sum(axis=1), dd['recov'].sum(axis=1)
+        return y, sens
 
-    times = np.arange(0,len(date_labels),1)
-    pdb.set_trace()
-    beta,gamma = 4,1.0
+
+def perform_inference(country):
+    dd, labels, labels_global = unpack_data()
+    dates, x, infected, susceptible = clean_by_country(country, dd, labels, case_thresh=10)
     
+    # For now, we use all but the last 'testdim' days as training data, and 
+    # the remaining days as testing data.
+    testdim = 2
+    
+    train = x[:-testdim]
+    test = x[-testdim:]
+
+    x_scale =  MinMaxScaler().fit_transform(train.reshape(-1,1)).flatten()
+
+    y_train = np.hstack((susceptible[:-testdim].reshape(-1,1), infected[:-testdim].reshape(-1,1)))
+    y_test = np.hstack((susceptible[-testdim:].reshape(-1,1), infected[-testdim:].reshape(-1,1)))
+
+    y0 = [y_train[0][0], y_train[0][1]]
+
+    sir_model = DE(
+        func=SIR,
+        times=train,
+        n_states=2,
+        n_theta=2, 
+        t0=0
+    )
 
     with pm.Model() as model:
-        sigma = pm.HalfCauchy('sigma', 1, shape=2)
+        print('Initializing Priors')
+        sigma = pm.HalfNormal('sigma', 1, shape=2)
 
         # R0 is bounded below by 1 because we see an epidemic has occured
-        R0 = pm.Bound(pm.Normal, lower=1)('R0', 2,3)
-        lam = pm.Lognormal('lambda',pm.math.log(2),2)
-        beta = pm.Deterministic('beta', lam*R0)
+        R0 = pm.Bound(pm.Normal, lower=1)('R0', 2, 3)
+        lmbda = pm.Normal('lambda', 0, 10)
+        delta = pm.Normal('delta', 0, 10)
+        beta = pm.Deterministic('beta', lmbda*R0)
+        
+        
+        print('Setting up model')
+        sir_curves = sir_model(y0=y0, theta=[beta, lmbda])
 
-        sir_curves = sir_model(y0=[0.99, 0.01], theta=[beta, lam])
+        Y = pm.Normal('Y', mu=sir_curves, sigma=sigma, observed=y_train)
 
-        Y = pm.Lognormal('Y', mu=pm.math.log(sir_curves), sd=sigma, observed=)
+        print('Starting sampling')
 
-        prior = pm.sample_prior_predictive()
-        trace = pm.sample(1000,tune=500, target_accept=0.9, cores=1)
-        posterior_predictive = pm.sample_posterior_predictive(trace)
+        trace = pm.sample(2000, tune=1000, cores=2, progressbar=True)
+        posterior_predictive = pm.sample_posterior_predictive(trace, progressbar=True)
 
-        data = az.from_pymc3(trace=trace, prior = prior, posterior_predictive = posterior_predictive)
+        pdb.set_trace()
 
-        fig, ax = plt.subplots()
-        az.plot_posterior(data,round_to=2, credible_interval=0.95)
-        fig.savefig('confirmed', dpi=1000, bbox_inches='tight')
+    return posterior_predictive, dates[:-testdim], y_train
+
+def plot(posterior_predictive, dates, y_train):
+    a = posterior_predictive['Y']
+    y0 = a[:,:,0]
+    y1 = a[:,:,1]
+
+    y0_mean = np.mean(y0, axis=0)
+    y1_mean = np.mean(y1, axis=0)
+
+    fig.savefig('pred_sus', dpi=1000, bbox_inches='tight')
+    fig.savefig('pred_inf', dpi=1000, bbox_inches='tight')
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    plt.plot(dates, y0_mean, 'g', label='susceptible')
+    plt.plot(dates, y1_mean, 'b', label='infected')
+    plt.xlabel('Dates')
+    plt.ylabel()
+    ax.legend()
+
+def main():
+    posterior_predictive, dates = perform_inference('US')
+    plot(posterior_predictive, dates)
+    pdb.set_trace()
 
 
 if __name__ == '__main__':
